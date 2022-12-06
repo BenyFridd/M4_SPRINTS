bdd06a0957435c768c5e562400b297f2c98b4d81
Gibeking <99186257+Gibeking@users.noreply.github.com>
2022-12-06 14:13:28 -0300

    .

--- SEMANA_07/EXERCICIOS/Front/app.js
+++ SEMANA_07/EXERCICIOS/Front/app.js
@@ -1,33 +1,39 @@
 const playerText = document.querySelector("#playerText");
 const ComputadorText = document.querySelector("#ComputadorText");
 const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelectorAll(".choiceBtn");
+const Botoes = document.querySelectorAll(".Botoes");
+
 let player;
 let Computador;
 let result;
 
-choiceBtns.forEach(button => button.addEventListener("click", () => {
+Botoes.forEach(button => button.addEventListener("click", () => {
 
     player = button.textContent;
     ComputadorTurn();
-    playerText.textContent = `Player: ${player}`;
-    ComputadorText.textContent = `Computador: ${Computador}`;
+
+    playerText.textContent = "Player: " + player;
+    ComputadorText.textContent = "Computador: " + Computador;
     resultText.textContent = checkWinner();
+
+    result = checkWinner();
+    console.log(result);
 }));
+
 
 function ComputadorTurn(){
 
-    const randNum = Math.floor(Math.random() * 3) + 1;
+    const Numaletorio = Math.floor(Math.random() * 3) + 1;
 
-    switch(randNum){
+    switch(Numaletorio){
       case 1:
-        Computador = "Pedra";
+        Computador == "Pedra";
         break;
       case 2:
-        Computador = "Papel";
+        Computador == "Papel";
         break;
       case 3:
-        Computador = "Tesoura";
+        Computador == "Tesoura";
         break;
     }
 }
@@ -35,13 +41,23 @@ function checkWinner(){
     if(player == Computador){
       return "Empate!";
     }
-    else if(Computador == "Pedra"){
-      return (player == "Papel") ? "Ganhou!" : "Perdeu!"
+    else if(Computador == "Pedra" && player == "Tesoura"){
+      return "Perdeu!";
     }
-    else if(Computador == "Papel"){
-      return (player == "Tesoura") ? "Ganhou!" : "Perdeu!"
+    else if(Computador == "Papel" && player == "Pedra"){
+      return "Perdeu!";
     }
-    else if(Computador == "Tesoura"){
-      return (player == "Pedra") ? "Ganhou!" : "Perdeu!"
+    else if(Computador == "Tesoura" && player == "Papel"){
+      return  "Perdeu!";
+    }  
+    else if (player == "Pedra" && Computador == "Tesoura"){
+      return "Ganhou!";
+    }
+    else if (player == "Papel" && Computador == "Pedra"){
+      return "Ganhou!";
+    }
+    else if (player == "Tesoura" && Computador == "Papel"){
+      return "Ganhou!";
     }
 }
+
